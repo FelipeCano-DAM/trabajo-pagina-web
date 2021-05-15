d14f9bce8aabe85ce631dea15006a7d2bbdd5067
FelipeCano-DAM <76010011+FelipeCano-DAM@users.noreply.github.com>
2021-05-15 13:47:57 +0200

    Add files via upload

--- gracias.php
+++ gracias.php
@@ -1,5 +1,5 @@
 <html>
     <body>
-        <?php echo "Hola $_POST['Nombre']"?>    
+        <?php echo "Hola $_POST['Nombre'] $_POST['Apellidos'], se le llamara al numero $_POST['Telefono'] cuando haya una solicitud de compra disponible. Un saludo"?>    
     </body>
 </html>
